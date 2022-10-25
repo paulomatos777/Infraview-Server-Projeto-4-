52f47e006870516dcbf3b1f661139f4eaed31120
Paulo Matos <paulomatos4554@gmail.com>
2022-10-25 07:30:06 -0300

    demand type model, migration and controller

--- InfraView/start/routes.ts
+++ InfraView/start/routes.ts
@@ -1,23 +1,3 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
-|
-*/
-
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
@@ -34,4 +14,8 @@ Route.group(() => {
 
 Route.group(() => {
   Route.resource('clients', 'ClientsController')
+})
+
+Route.group(() => {
+  Route.resource('demand-types', 'DemandTypesController')
 })
